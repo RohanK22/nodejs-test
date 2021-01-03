78f7d242c3e6d0a0a9069685e641e17fa3bc88aa
RohanK22 <rohank2205@gmail.com>
2021-01-03 15:47:28 +0000

    A simple node example

--- node-examples/index.js
+++ node-examples/index.js
@@ -2,3 +2,19 @@ var rect = {
     perimeter: (x, y) => 2 * (x + y),
     area: (x, y) => x * y,
 };
+
+function solveRect(l, b) {
+    if (l < 0 || b < 0) {
+        console.log('Wrong dimensions!');
+    } else {
+        console.log(
+            'Area: ' +
+                rect.area(l, b) +
+                '\n' +
+                'Perimeter: ' +
+                rect.perimeter(l, b)
+        );
+    }
+}
+
+solveRect(2, 4);
